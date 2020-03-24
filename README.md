75abc7ac825bdbddafa57d928ad22476a2a7c2d3
Mohan krishna Alavala <alavalamohan@gmail.com>
2020-03-24 18:26:58 -0500

    modefied

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # node-server
+## node.js
+Node.js is an open-source, cross-platform, JavaScript runtime environment that executes JavaScript code outside of a web browser. 
+
+## Advantages of node.js
+1. Node.js offers an Easy Scalability
+2. Easy to Learn
+3. Node.js is used as a Single Programming Language
+4. The Benefit of Fullstack JS
+5. Known for Offering High Performance
+
+## Reference
+- [mindinventoey](https://www.mindinventory.com/blog/)
+- [wikipedia](https://en.wikipedia.org/wiki/Node.js)
+- [node.js](https://nodejs.org/en/)
--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const http = require('http')  // require built-in Node.js http package
 
 // Use hosting values if available, otherwise default 
 const hostname = process.env.hostname || '192.168.0.22' // allow remote access
-const port = process.env.PORT || 1522
+const port = process.env.PORT || 3022
 
 // define our server
 const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello World! This is text - we can respond with HTML, JSON, and more :)\n')
+  res.end('Hi Guys, This is my first server side program :D\n')
 })
 
 // start listening
